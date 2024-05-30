325873bc10eee6a33f30529260c8b14f3aee229f
小陈 <jamiechen.778@gmail.com>
2024-05-30 23:14:52 +0800

    Update (#6)
    * 查询订单详情 (#4)
    
    * Upload File By OSS (#5)
    
    Upload file to OSS

--- yolo-server/src/main/java/com/yolo/controller/UserController.java
+++ yolo-server/src/main/java/com/yolo/controller/UserController.java
@@ -2,7 +2,6 @@ package com.yolo.controller;
 
 import com.yolo.constant.MessageConstant;
 import com.yolo.pojo.dto.UserLoginDTO;
-import com.yolo.pojo.dto.UserUpdateDTO;
 import com.yolo.pojo.vo.UserLoginVO;
 import com.yolo.result.Result;
 import com.yolo.service.UserService;
@@ -30,6 +29,7 @@ public class UserController {
 
     /**
      * 微信登录
+     *
      * @return
      */
     @PostMapping("/user/login")
@@ -40,11 +40,6 @@ public class UserController {
         return Result.success(userLoginVO);
     }
 
-    /**
-     * 文件上传
-     * @param file
-     * @return
-     */
     @PostMapping("/common/upload")
     @ApiOperation("上传文件")
     public Result<String> upload(MultipartFile file) {
@@ -62,19 +57,5 @@ public class UserController {
             log.info("文件上传失败，{}", file);
         }
         return Result.error(MessageConstant.UPLOAD_FAILED);
-    }
-
-
-    /**
-     * 更新用户的信息
-     * @param userUpdateDTO
-     * @return
-     */
-    @PostMapping("/user/update")
-    @ApiOperation("更新用户信息")
-    public Result update(UserUpdateDTO userUpdateDTO) {
-        log.info("更新用户信息：{}",userUpdateDTO);
-        userService.update(userUpdateDTO);
-        return Result.success();
     }
 }
