dc211fb12463051a8eb56541d602127c3b686391
Jamie <jamiechen.778@gmail.com>
2024-05-30 21:08:38 +0800

    :Upload file to OSS

---
+++ yolo-server/src/main/java/com/yolo/config/OssConfiguration.java
@@ -0,0 +1,26 @@
+package com.yolo.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.yolo.properties.AliOssProperties;
+import com.yolo.utility.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ */
+@Slf4j
+@Configuration
+public class OssConfiguration {
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里云文件上传工具类对象：{}",aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(), aliOssProperties.getBucketName());
+    }
+}
--- yolo-server/src/main/java/com/yolo/controller/UserController.java
+++ yolo-server/src/main/java/com/yolo/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.yolo.controller;
+
+import com.yolo.constant.MessageConstant;
 import com.yolo.pojo.dto.UserLoginDTO;
 import com.yolo.pojo.vo.UserLoginVO;
 import com.yolo.result.Result;
 import com.yolo.service.UserService;
+import com.yolo.utility.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
 
 
 @RestController
@@ -17,17 +24,38 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
     /**
      * 微信登录
+     *
      * @return
      */
     @PostMapping("/user/login")
     @ApiOperation("用户微信登录")
     public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO) {
-        log.info("微信用户 : {}登录",userLoginDTO);
+        log.info("微信用户 : {}登录", userLoginDTO);
         UserLoginVO userLoginVO = userService.wxLogin(userLoginDTO);
         return Result.success(userLoginVO);
     }
 
+    @PostMapping("/common/upload")
+    @ApiOperation("上传文件")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+        try {
+            String originalFilename = file.getOriginalFilename();
+            // 截取原始文件名后缀
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            // 构造新文件的名称
+            String objectName = UUID.randomUUID() + extension;
+            // 文件请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.info("文件上传失败，{}", file);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
 }
