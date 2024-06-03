6b1cd51e5f704650b6c875cf40a8218716d7a10f
Jamie <jamiechen.778@gmail.com>
2024-06-03 22:16:09 +0800

    修改json序列器的时间格式

--- yolo-common/src/main/java/com/yolo/json/JacksonObjectMapper.java
+++ yolo-common/src/main/java/com/yolo/json/JacksonObjectMapper.java
@@ -26,7 +26,7 @@ public class JacksonObjectMapper extends ObjectMapper {
 
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
     //public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm";
+    public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
     public JacksonObjectMapper() {
